af72cdbd88a95f8d8e0f29fd45f9c54690ebaa69
Rafał <rafal_gluszek@interia.pl>
2019-07-02 14:21:00 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log('siema!');
+const name = "Rafał";
+const age = 36;
+console.log(name);
+console.log(age);
+console.log(`Nazywam się ${name} 
+i mam ${age} lat`);
+
+let value = null;
+console.log(value);
